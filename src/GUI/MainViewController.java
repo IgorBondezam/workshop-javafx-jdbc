c025aed5fbeb0514282ba09d7e7a58b5fa9832b0
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-31 13:41:53 -0300

    DepartmentList view Design

--- src/GUI/MainViewController.java
+++ src/GUI/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction(){
-        System.out.println("oonMenuItemDepartmentAction");
+        loadView("/GUI/DepartmentList.fxml");
     }
 
     @FXML
