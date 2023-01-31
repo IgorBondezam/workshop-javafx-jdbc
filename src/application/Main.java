912276d6a7ed6469ac03e6ebdfe8abbcb36b0f54
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-31 13:01:45 -0300

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,30 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) {
         try {
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root,400,400);
-
-            primaryStage.setScene(scene);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/MainView.fxml"));
+            Parent parent = loader.load();
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
-        } catch(Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 
     public static void main(String[] args) {
         launch(args);
